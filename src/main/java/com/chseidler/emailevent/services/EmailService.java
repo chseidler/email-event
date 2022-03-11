28310ba7419463e49872ee6207d5bf2d919c79a7
Christian Seidler <ch.cseidler@gmail.com>
2022-03-10 22:32:48 -0300

    camada serviço definida

--- src/main/java/com/chseidler/emailevent/services/EmailService.java
+++ src/main/java/com/chseidler/emailevent/services/EmailService.java
@@ -1,9 +1,15 @@
 package com.chseidler.emailevent.services;
 
+import com.chseidler.emailevent.enums.StatusEmail;
 import com.chseidler.emailevent.models.EmailModel;
 import com.chseidler.emailevent.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
@@ -11,6 +17,27 @@ public class EmailService {
     @Autowired
     EmailRepository emailRepository;
 
-    public void sendEmail(EmailModel emailModel) {
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public EmailModel sendEmail(EmailModel emailModel) {
+
+        emailModel.setSendDateEmail(LocalDateTime.now());
+
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+
+        } catch (MailException e) {
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            return emailRepository.save(emailModel);
+        }
     }
 }
